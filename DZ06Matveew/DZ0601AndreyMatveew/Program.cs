ede39a5e0602c4c61db4cb84a9c780ba04647b8a
Andrew Matveew <andreyxomak007@gmail.com>
2020-09-13 14:10:16 +0300

    1

---
+++ DZ06Matveew/DZ0601AndreyMatveew/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace DZ0601AndreyMatveew
+{
+    /* Матвеев Андрей. Задача 1.
+    Изменить программу вывода таблицы функции так, чтобы можно было передавать функции типа double (double, double). 
+    Продемонстрировать работу на функции с функцией a*x^2 и функцией a*sin(x).
+    */
+    public delegate double Fun(double x, double y);
+    class Program
+    {
+        // Создаем метод, который принимает делегат
+        // На практике этот метод сможет принимать любой метод
+        // с такой же сигнатурой, как у делегата
+        public static void Table(Fun F, double x, double b,double y)
+        {
+            Console.WriteLine("----- X ----- Y -----");
+            while (x <= b)
+            {
+                Console.WriteLine("| {0,8:0.000} | {1,8:0.000} |", x, F(x,y));
+                x += 1;
+            }
+            Console.WriteLine("---------------------");
+        }
+        // Создаем метод для передачи его в качестве параметра в Table
+        public static double MyFunc(double x, double y)
+        {
+            return y * Math.Pow(x,2);
+        }
+
+        public static double MyFuncSin(double x, double y)
+        {
+            return y * Math.Sin(x);
+        }
+
+        static void Main()
+        {
+            //Продемонстрировать работу на функции с функцией a* x^2
+            Console.WriteLine("Таблица функции MyFunc:");
+            Table(MyFunc, -2, 2,2);
+
+            //Продемонстрировать работу на функции с функцией a*sin(x)
+            Console.WriteLine("Таблица функции MyFuncSin:");
+            Table(MyFuncSin, -2, 2, 2);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ DZ06Matveew/DZ0602AndreyMatveew/Program.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+/* Матвеев Андрей. Задача 2.
+Модифицировать программу нахождения минимума функции так, чтобы можно было передавать функцию в виде делегата. 
+а) Сделать меню с различными функциями и представить пользователю выбор, 
+для какой функции и на каком отрезке находить минимум. 
+Использовать массив (или список) делегатов, в котором хранятся различные функции.
+б) *Переделать функцию Load, чтобы она возвращала массив считанных значений. 
+Пусть она возвращает минимум через параметр (с использованием модификатора out).
+*/
+namespace DZ0602AndreyMatveew
+{
+    class Program
+    {
+        public delegate double Fun(double x);
+        public static double FuncFirst(double x)
+        {
+            return x * x - 50 * x + 10;
+        }
+        public static double FuncSecond(double x)
+        {
+            return x * x - 50;
+        }
+        public static double FuncThird(double x)
+        {
+            return x * x  + 10;
+        }
+
+        public static void ChoseFunc()
+        {
+            double min;
+            string chose = "";
+
+            do
+            {
+                Console.WriteLine("Выберите функцию:");
+                Console.WriteLine("1 - Функция X * X - 50 * x + 10:");
+                Console.WriteLine("2 - Функция X * X - 50:");
+                Console.WriteLine("3 - Функция X * X  + 10:");
+                chose = Console.ReadLine();
+                switch (chose)
+                {
+                    case "1":
+                        Console.WriteLine("Выбрана функция X * X - 50 * x + 10");
+                        SaveFunc(FuncFirst, "data.bin", -100, 100, 0.5);
+                        Load("data.bin", out min);
+                        break;
+                    case "2":
+                        Console.WriteLine("Выбрана функция X * X - 50");
+                        SaveFunc(FuncSecond, "data.bin", -100, 100, 0.5);
+                        Load("data.bin", out min);
+                        break;
+                    case "3":
+                        Console.WriteLine("Выбрана функция X * X  + 10");
+                        SaveFunc(FuncThird, "data.bin", -100, 100, 0.5);
+                        Load("data.bin", out min);
+                        break;
+                    default:
+                        Console.WriteLine("Номера такой функции не существует");
+                        break;
+                }
+            } while (chose != "e");
+        }
+        public static void SaveFunc(Fun F,string fileName, double a, double b, double h)
+        {
+            FileStream fs = new FileStream(fileName, FileMode.Create, FileAccess.Write);
+            BinaryWriter bw = new BinaryWriter(fs);
+            while (a <= b)
+            {
+                bw.Write(F(a));
+                a += h;
+            }
+            bw.Close();
+            fs.Close();
+        }
+        public static List<double> Load(string fileName,out double min)
+        {
+            FileStream fs = new FileStream(fileName, FileMode.Open, FileAccess.Read);
+            BinaryReader bw = new BinaryReader(fs);
+            List<double> d = new List<double>();
+            min = double.MaxValue;
+
+            for (int i = 0; i < fs.Length / sizeof(double); i++)
+            {
+                // Считываем значение и переходим к следующему
+                d.Add(bw.ReadDouble());
+                if (d[d.Count - 1] < min) min = d[d.Count - 1] - 1;
+            }
+
+            Console.WriteLine("Данные функции: ");
+            foreach (var el in d)
+            {
+                Console.WriteLine(el);
+            }
+            Console.WriteLine($"Вывод min: {min}");
+
+            bw.Close();
+            fs.Close();
+            return d;
+        }
+        static void Main(string[] args)
+        {
+            ChoseFunc();
+            Console.ReadKey();
+        }
+    }
+}
